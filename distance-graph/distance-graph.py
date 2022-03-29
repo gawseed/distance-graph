5a291a2b30e40b88420b37e486fa19231b136374
Erin Szeto <erinszet@usc.edu>
2022-03-29 14:07:53 -0700

    update main() returns

--- distance-graph/distance-graph.py
+++ distance-graph/distance-graph.py
@@ -1497,8 +1497,8 @@ def main():
     else:
         cmd2template = None
 
-    weightDic,cmdIPsDic,sourceDic,cmdToArray,cmd2template,templates,cmd2templateCount = get_info(file_args, output_names, cmd2template, args)
-    G,weighted_edges,labels,clusters,pos = draw_networkx(args,output_names,weightDic,cmdIPsDic,sourceDic,cmdToArray,cmd2template,templates,cmd2templateCount)
+    weightDic,cmdIPsDic,sourceDic,cmdToArray,cmd2template,templates,cmd2templateCount,old_templates = get_info(file_args, output_names, cmd2template, args)
+    G,weighted_edges,labels,clusters,pos = draw_networkx(args,output_names,weightDic,cmdIPsDic,sourceDic,cmdToArray,cmd2template,cmd2templateCount,old_templates)
 
     ## save NetworkX graph position file to pickle file
     if (args.position_file):
