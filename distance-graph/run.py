d76336748cf08c3a25466b9833d42bcb8a01f72a
Erin Szeto <erinszet@usc.edu>
2022-04-20 15:02:17 -0700

    use Data labelDic

--- distance-graph/run.py
+++ distance-graph/run.py
@@ -140,12 +140,12 @@ def get_info2(args, cmd2template):
     login_index = False
 
     if args.id_name != '':
-        # cmdIPsDic = data.labelDic
-        # sourceDic = data.sourceDic
-        cmdIPsDic,sourceDic = get_cmdIPsDic(args.file_args,data.loggedInOnly,args.id_name,login_index, temporal)
+        cmdIPsDic = data.labelDic
+        sourceDic = data.sourceDic
+        # cmdIPsDic,sourceDic = get_cmdIPsDic(args.file_args,data.loggedInOnly,args.id_name,login_index, temporal)
     else:
-        # labelDic = data.labelDic
-        labelDic = get_labelDic(args.file_args,login_index,temporal)
+        labelDic = data.labelDic
+        # labelDic = get_labelDic(args.file_args,login_index,temporal)
         cmdIPsDic = None
 
     got_unique_cmds = False
