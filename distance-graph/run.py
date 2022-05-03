52b7e23f1d5847192cd4bc5894b43342ac342dc8
Erin Szeto <erinszet@usc.edu>
2022-05-03 14:20:40 -0700

    remove comments

--- distance-graph/run.py
+++ distance-graph/run.py
@@ -8,21 +8,6 @@ import pickle
 
 
 def get_info2(args, cmd2template):
-    """ Cleans and transforms input data according to user arguments. Finds weights and labels of nodes.
-        Return four dictionaries: (1) weights between commands, (2) IPs that ran commands, (3) sources for each command,
-        and (4) command to array style string
-    Input:
-        file_args (list) - list of file argument lists with format [filename, node name, label name, identifier name]
-        output_names (list) - list of output names to use for node, label, and identifier
-        cmd2template (dict) - maps templatizable commands to highest degree template
-        args (class) - ArgumentParser class with input arguments
-    Output:
-        weightDic (dict) - key: pair of commands (tuple) / value: weight (float)
-        cmdIPsDic (dict) - key: command (str) / value: dictionary with key: source (str) & value: IPs that ran command (list)
-        sourceDic (dict) - key: command (str) / value: source label (str)
-        cmdToArray (dict) - key: command (str) / value: array style command (str)
-        cmd2template (dict) - key: command (str) / value: template (tuple)
-    """
     templates_class = cmd2template
     data = Data(args)
 
@@ -48,19 +33,6 @@ def get_info2(args, cmd2template):
     return data,templates_class
 
 def draw_networkx2(args,data,templates):
-    """ Finds the weighted edges and plots the NetworkX graph. Obtains labels for nodes and cluster IDs for connected nodes
-    Input:
-        args (argument parser) - parser of command line arguments,
-        weightDic (dict) - weights for each pair of commands, cmdIPsDic (dict) - maps command to a dictionary that has source and IPs that ran the command,
-        cmdIPsDic (dict) - maps command to dict that contains source and IP addresses
-        sourceDic (dict) - maps command to source label, cmdToArray (dict) - maps command to array style command,
-        cmdToArray (dict) - maps command (str) to array style command (str)
-    Output:
-        G (NetworkX graph) - labeled graph with weighted edges,
-        weighted_edges (list) - list of tuples containing command pair and weight,
-        labels (dict) - maps command node to integer,
-        clusters (dict) - key: command node (str) / value: cluster ID (int)
-    """
     networkgraph = NetworkGraph(args,data,templates)
 
     labels = networkgraph.labels
@@ -74,32 +46,16 @@ def draw_networkx2(args,data,templates):
 
 def main():
     args = Arguments()
-    # args = parse_args()
-
-    # ## get list of file args and remove None input files args that were not specified
-    # file_args = list(filter(None,[args.input_file1, args.input_file2, args.input_file3, args.input_file4]))
-    # output_names = args.output_names
-
-    # check_fileArgs(file_args, output_names)
 
     if args.args.templatize:
-        # temporal = args.temporal
-        # stopwords = args.stopwords
         if args.args.stopwords == None:
             raise Exception('Stopwords file is missing. The stopwords file is required for templatization.')
 
-        # global _NIX_COMMANDS
-        # if _NIX_COMMANDS is None:
-        #     _NIX_COMMANDS = set(open(stopwords).read().split('\n'))
-
         cmd2template = Templates(args.args.stopwords, args.file_args, args.args.temporal)
-        # cmd2template = get_cmd2template(file_args,temporal)
     else:
         cmd2template = None
 
     data,templates_class = get_info2(args, cmd2template)
-    # weightDic,cmdIPsDic,sourceDic,cmdToArray,cmd2template,templates,cmd2templateCount,old_templates = get_info(file_args, output_names, cmd2template, args)
-    # G,weighted_edges,labels,clusters,pos = draw_networkx(args,args.output_names,weightDic,cmdIPsDic,sourceDic,cmdToArray,cmd2template,cmd2templateCount,old_templates)
     G,weighted_edges,labels,clusters,pos = draw_networkx2(args,data,templates_class)
 
     ## save NetworkX graph position file to pickle file
@@ -113,7 +69,6 @@ def main():
             pickle.dump(output_labels, open(args.args.labels_file, "wb"))
         else:
             output_labels = {cmd:{'label':label, 'template':"N/A"} for cmd,label in labels.items()}
-            # pickle.dump(labels, open(args.labels_file, "wb" ))
 
     ## create edge list to FSDB file
     if (args.args.edge_list):
