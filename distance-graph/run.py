6255ec4ae34959351264410db2839171c9fa8aea
Erin Szeto <erinszet@usc.edu>
2022-05-05 12:09:10 -0700

    change variable names

--- distance-graph/run.py
+++ distance-graph/run.py
@@ -7,32 +7,29 @@ import pyfsdb
 import pickle
 
 
-def get_info2(args, cmd2template):
-    templates_class = cmd2template
+def get_info(args, templates):
     data = Data(args)
 
-    if cmd2template:
-        data.find_unique_templatized_cmds(args, cmd2template, args.args.temporal)
-
-        cmd2template = cmd2template.cmd2template
+    if templates:
+        data.find_unique_templatized_cmds(args, templates, args.args.temporal)
 
         if args.args.template_nodes:
-            data.get_template_nodes(templates_class, args)
-            templates_class.calculate_template_counts(data.df, args.node_name, data.unique_cmds)
+            data.get_template_nodes(templates, args)
+            templates.calculate_template_counts(data.df, args.node_name, data.unique_cmds)
             
             if args.args.labels:
                 labels = pickle.load(open(args.args.labels,"rb"))
-                data.update_representative_cmd(labels,templates_class)
+                data.update_representative_cmd(labels,templates)
 
-                templates_class.cmd2template_count = data.remap_dic(templates_class.cmd2template_count, data.cmd_to_old_label, 'cmd')
+                templates.cmd2template_count = data.remap_dic(templates.cmd2template_count, data.cmd_to_old_label, 'cmd')
     
     data.get_unique_cmds()
     data.calculate_weights()
     data.update_source_dic(args)
 
-    return data,templates_class
+    return data,templates
 
-def draw_networkx2(args,data,templates):
+def draw_networkx(args,data,templates):
     networkgraph = NetworkGraph(args,data,templates)
     networkgraph.plot_networkx(args, templates)
 
@@ -45,12 +42,12 @@ def main():
         if args.args.stopwords == None:
             raise Exception('Stopwords file is missing. The stopwords file is required for templatization.')
 
-        cmd2template = Templates(args.args.stopwords, args.file_args, args.args.temporal)
+        templates = Templates(args.args.stopwords, args.file_args, args.args.temporal)
     else:
-        cmd2template = None
+        templates = None
 
-    data,templates_class = get_info2(args, cmd2template)
-    networkgraph = draw_networkx2(args,data,templates_class)
+    data,templates = get_info(args, templates)
+    networkgraph = draw_networkx(args,data,templates)
 
     ## save NetworkX graph position file to pickle file
     if (args.args.position_file):
@@ -59,7 +56,7 @@ def main():
     ## save labels dict to pickle file
     if (args.args.labels_file):
         if (args.args.template_nodes):
-            output_labels = {cmd:{'label':label, 'template':templates_class.cmd2template[cmd]} for cmd,label in networkgraph.labels.items()}
+            output_labels = {cmd:{'label':label, 'template':templates.cmd2template[cmd]} for cmd,label in networkgraph.labels.items()}
             pickle.dump(output_labels, open(args.args.labels_file, "wb"))
         else:
             output_labels = {cmd:{'label':label, 'template':"N/A"} for cmd,label in networkgraph.labels.items()}
@@ -74,8 +71,8 @@ def main():
                 cluster_id = networkgraph.clusters[cmd1]
                 num1 = networkgraph.labels[cmd1]
                 num2 = networkgraph.labels[cmd2]
-                template1 = ' '.join(templates_class.cmd2template[cmd1])
-                template2 = ' '.join(templates_class.cmd2template[cmd2])
+                template1 = ' '.join(templates.cmd2template[cmd1])
+                template2 = ' '.join(templates.cmd2template[cmd2])
                 outh.append([cluster_id,round(weight,3),num1,num2,cmd1,cmd2,template1,template2])
             outh.close()
         else:
@@ -94,7 +91,7 @@ def main():
             outh = pyfsdb.Fsdb(out_file=args.args.cluster_list)
             outh.out_column_names=['cluster_id','command','template']
             for cmd,cluster_id in networkgraph.clusters.items():
-                template = ' '.join(templates_class.cmd2template[cmd])
+                template = ' '.join(templates.cmd2template[cmd])
                 outh.append([cluster_id,cmd,template])
             outh.close()
         else:
@@ -111,7 +108,7 @@ def main():
             outh.out_column_names=['template','command','node','label']
             template_list = []
             for cmd in networkgraph.clusters.keys():
-                template = ' '.join(templates_class.cmd2template[cmd])
+                template = ' '.join(templates.cmd2template[cmd])
                 node = networkgraph.labels[cmd]
                 if data.sourceDic != {}:
                     label = data.sourceDic[cmd]
@@ -131,7 +128,7 @@ def main():
             outh = pyfsdb.Fsdb(out_file=args.args.templatecmd_list)
             outh.out_column_names=['template','command']
             tc_list = []
-            for temp,cmds in templates_class.template2cmd.items():
+            for temp,cmds in templates.template2cmd.items():
                 template = ' '.join(temp)
                 cmds = sorted(set(cmds))
                 cmds = [cmd[2:-2] for cmd in cmds]
